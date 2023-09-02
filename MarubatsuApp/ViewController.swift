809648b12dbb1d12d980a68ed07270a72af15be1
Nao0811 <naoto.7010.811@icloud.com>
2023-09-02 11:45:52 +0900

    講義の分は完成！

--- MarubatsuApp/ViewController.swift
+++ MarubatsuApp/ViewController.swift
@@ -9,11 +9,83 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    //表示中の問題番号を格納
+    var currentQuestionNum: Int = 0
+    
+    // 問題
+        let questions: [[String: Any]] = [
+            [
+                "question": "iPhoneアプリを開発する統合環境はZcodeである",
+                "answer": false
+            ],
+            [
+                "question": "Xcode画面の右側にはユーティリティーズがある",
+                "answer": true
+            ],
+            [
+                "question": "UILabelは文字列を表示する際に利用する",
+                "answer": true
+            ]
+        ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        showQuestion()
     }
-
+    //問題を表示する関数
+    func showQuestion(){
+        let question = questions[currentQuestionNum]
+       
+        if let que = question["question"] as? String {
+            questionLabel.text = que
+        }
+        
+    }
+    //回答チェックの関数、正解なら次の問題を表示
+    func checkAnswer(yourAnswer: Bool){
+        let question = questions[currentQuestionNum]
+        
+        if let ans = question["answer"]as? Bool{
+            if yourAnswer == ans {
+                //正解のときの処理
+                //currentquestionNumに1を足して次の問題に進む
+                currentQuestionNum += 1
+                showAlert(message: "正解！")
+            }else {
+                //不正解のときの処理
+                showAlert(message: "不正解！")
+            }
+        }else {//答えがないとき
+            print("答えが入っていません")
+            return
+        }
+        //correntquestionNumの値が問題数以上なら最初の問題へ
+        if currentQuestionNum >= questions.count {
+            currentQuestionNum = 0
+        }
+        
+        showQuestion()//正解のときは次の問題表示、不正解→同じ問題
+    }
+    
+    func showAlert(message: String){
+        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        
+        let close = UIAlertAction(title: "閉じる", style: .cancel)
+        alert.addAction(close)
+        present(alert, animated: true)
+    }
+    
+    
+    @IBAction func tappedNoButton(_ sender: Any) {
+        checkAnswer(yourAnswer: false)
+    }
+    
+    @IBAction func tappedYesButton(_ sender: Any) {
+        checkAnswer(yourAnswer: true)
+    }
+    
 
 }
 
